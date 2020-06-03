39629cdedaccb444dd354c5dc9381c2ef74a1a28
Pavel Chuykin <p7chkn@yandex.ru>
2020-06-03 20:01:31 +0700

    put things where it belongs

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -16,7 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',    
+    'rest_framework',
+    'django_filters',
     'api',
 ]
 
@@ -93,6 +94,9 @@ REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [
             'rest_framework_simplejwt.authentication.JWTAuthentication',
         ],
+        'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',        
+    ),
         
 
     }
